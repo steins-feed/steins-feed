1729dd2442f841113f1053c84ada84f6434e6132
Hans Yu <hans.yu@outlook.de>
2018-05-07 17:34:40 +0100

    Check for uniqueness.

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ db_exists = os.path.isfile(db_name)
 conn = sqlite3.connect("steins.db")
 c = conn.cursor()
 if not db_exists:
-    # TODO Check for uniqueness.
     # TODO Table for authors.
     # TODO Table for categories.
     c.execute("CREATE TABLE Items (ItemID INT AUTO_INCREMENT, Title TEXT NOT NULL, Published DATETIME NOT NULL, Summary MEDIUMTEXT, Link TEXT NOT NULL, PRIMARY KEY (ItemID))")
@@ -24,7 +23,8 @@ if not db_exists:
 for feed_it in feeds:
     d = feedparser.parse(feed_it)
     for item_it in d['items']:
-        c.execute("INSERT INTO Items (Title, Published, Summary, Link) VALUES (?, ?, ?, ?)", (item_it["title"], time.strftime("%Y-%m-%d %H:%M:%S", item_it["published_parsed"]), item_it["summary"], item_it["link"]))
+        if c.execute("SELECT COUNT(*) FROM Items WHERE Title=?", (item_it["title"], )).fetchone()[0] == 0:
+            c.execute("INSERT INTO Items (Title, Published, Summary, Link) VALUES (?, ?, ?, ?)", (item_it["title"], time.strftime("%Y-%m-%d %H:%M:%S", item_it["published_parsed"]), item_it["summary"], item_it["link"], ))
 
 # SQLite.
 for row_it in c.execute("SELECT Published FROM Items ORDER BY Published DESC"):
