eb83900d11ea09eb475ff7db88b14149009ffbe2
Hans Yu <hans.yu@outlook.de>
2018-05-07 16:36:56 +0100

    Bug fix. Save changes.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import os.path
 import feedparser
 import sqlite3
 
+feeds = []
+feeds.append("https://www.theguardian.com/uk/technology/rss")
+feeds.append("https://www.theatlantic.com/feed/channel/technology/")
+
 # SQLite.
 db_name = "steins.db"
 db_exists = os.path.isfile(db_name)
@@ -13,16 +17,16 @@ if not db_exists:
     # TODO Check for uniqueness.
     # TODO Table for authors.
     # TODO Table for categories.
-    #c.execute("CREATE TABLE Items (ItemID INT AUTO_INCREMENT, Title TEXT NOT NULL, Creator TEXT NOT NULL, Date DATETIME NOT NULL, Description MEDIUMTEXT, Link TEXT NOT NULL, PRIMARY KEY (ItemID))")
-    c.execute("CREATE TABLE Items (ItemID INT AUTO_INCREMENT, Title TEXT NOT NULL, Creator TEXT NOT NULL, Description MEDIUMTEXT, Link TEXT NOT NULL, PRIMARY KEY (ItemID))")
+    c.execute("CREATE TABLE Items (ItemID INT AUTO_INCREMENT, Title TEXT NOT NULL, Description MEDIUMTEXT, Link TEXT NOT NULL, PRIMARY KEY (ItemID))")
 
 # Guardian.
-d = feedparser.parse("https://www.theguardian.com/uk/technology/rss")
-for item_it in d['items']:
-    #c.execute("INSERT INTO Items (Title, Creator, Date, Description, Link) VALUES (?, ?, ?, ?, ?)", (item_it["title"], item_it["dc:creator"], item_it["dc:date"], item_it["description"], item_it["link"]))
-    c.execute("INSERT INTO Items (Title, Creator, Description, Link) VALUES (?, ?, ?, ?)", (item_it["title"], item_it["author"], item_it["description"], item_it["link"]))
+for feed_it in feeds:
+    d = feedparser.parse(feed_it)
+    for item_it in d['items']:
+        c.execute("INSERT INTO Items (Title, Description, Link) VALUES (?, ?, ?)", (item_it["title"], item_it["description"], item_it["link"]))
 
 # SQLite.
 for row_it in c.execute("SELECT Title FROM Items"):
     print(row_it)
+conn.commit()
 conn.close()
