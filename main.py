9f99c0f1b2813c55c60a8164047d825f1c8dee17
Hans Yu <hans.yu@outlook.de>
2018-05-07 18:48:13 +0100

    Add column for source.

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ if not db_exists:
     # TODO Table for authors.
     # TODO Table for categories.
     # TODO Denote source.
-    c.execute("CREATE TABLE Items (ItemID INT AUTO_INCREMENT, Title TEXT NOT NULL, Published DATETIME NOT NULL, Summary MEDIUMTEXT, Link TEXT NOT NULL, PRIMARY KEY (ItemID))")
+    c.execute("CREATE TABLE Items (ItemID INT AUTO_INCREMENT, Title TEXT NOT NULL, Published DATETIME NOT NULL, Summary MEDIUMTEXT, Source TEXT NOT NULL, Link TEXT NOT NULL, PRIMARY KEY (ItemID))")
 
 d = feedparser.parse("https://www.theguardian.com/uk/technology/rss")
 for item_it in d['items']:
     if c.execute("SELECT COUNT(*) FROM Items WHERE Title=? AND Published=?", (item_it['title'], time.strftime("%Y-%m-%d %H:%M:%S", item_it['published_parsed']))).fetchone()[0] == 0:
-        c.execute("INSERT INTO Items (Title, Published, Summary, Link) VALUES (?, ?, ?, ?)", (item_it['title'], time.strftime("%Y-%m-%d %H:%M:%S", item_it['published_parsed']), item_it['summary'], item_it['link'], ))
+        c.execute("INSERT INTO Items (Title, Published, Summary, Source, Link) VALUES (?, ?, ?, ?, ?)", (item_it['title'], time.strftime("%Y-%m-%d %H:%M:%S", item_it['published_parsed']), item_it['summary'], "Guardian Technology", item_it['link'], ))
 
 d = feedparser.parse("https://www.theatlantic.com/feed/channel/technology/")
 for item_it in d['items']:
     if c.execute("SELECT COUNT(*) FROM Items WHERE Title=? AND Published=?", (item_it['title'], time.strftime("%Y-%m-%d %H:%M:%S", item_it['published_parsed']))).fetchone()[0] == 0:
-        c.execute("INSERT INTO Items (Title, Published, Summary, Link) VALUES (?, ?, ?, ?)", (item_it['title'], time.strftime("%Y-%m-%d %H:%M:%S", item_it['published_parsed']), item_it['content'][1]['value'], item_it['link'], ))
+        c.execute("INSERT INTO Items (Title, Published, Summary, Source, Link) VALUES (?, ?, ?, ?, ?)", (item_it['title'], time.strftime("%Y-%m-%d %H:%M:%S", item_it['published_parsed']), item_it['content'][1]['value'], "Atlantic Technology", item_it['link'], ))
 
 conn.commit()
 conn.close()
@@ -46,8 +46,8 @@ f.write("<title>Stein's Feed</title>\n")
 f.write("</head>\n")
 f.write("<body>\n")
 for row_it in c.execute("SELECT * FROM Items ORDER BY Published DESC"):
-    f.write("<h2><a href=\"{}\">{}</a></h2>\n".format(row_it[4], row_it[1]))
-    f.write("<p>Date: {}</p>".format(row_it[2]))
+    f.write("<h2><a href=\"{}\">{}</a></h2>\n".format(row_it[5], row_it[1]))
+    f.write("<p>Source: {}. Published: {}</p>".format(row_it[4], row_it[2]))
     f.write("{}".format(row_it[3]))
     f.write("<hr>\n")
 f.write("</body>\n")
