8f81c2356884859337f9a7c0619507718d29e2e8
Hans Yu <hans.yu@outlook.de>
2019-04-20 17:54:12 +0200

    Naive Bayes PHP and Python script for server.

---
+++ naive_bayes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+from steins_magic import handle_naive_bayes
+from urllib.parse import parse_qsl
+
+qd = dict(parse_qsl(sys.argv[1]))
+print(handle_naive_bayes(qd))
