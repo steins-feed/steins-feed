91db2e936cb215467376e3e3c4833eff4f73db27
Hans Yu <hans.yu@outlook.de>
2019-06-07 19:38:45 +0200

    Index page.

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-    if( !$_GET["page"] ) {
-        $_GET["page"] = 0;
-    }
+if( !$_GET["page"] ) {
+    $_GET["page"] = 0;
+}
+$get_query = http_build_query($_GET);
+$python_cmd = <<<EOT
+import sys
 
-    // system('env PYTHONIOENCODING=UTF-8 python3 index.py "' . http_build_query($_GET) . '" > index.log 2>&1'); // DEBUG.
-    system('env PYTHONIOENCODING=UTF-8 python3 index.py "' . http_build_query($_GET) . '"');
+from steins_server import handle_page
+from urllib.parse import parse_qsl
+
+qd = dict(parse_qsl(sys.argv[1]))
+print(handle_page(qd))
+EOT;
+$bash_cmd = "env PYTHONIOENCODING=UTF-8 python3 -c \"$python_cmd\" \"$get_query\"";
+// system($bash_cmd . ' >> index.log 2>&1'); // DEBUG.
+system($bash_cmd);
 ?>
--- index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-from steins_server import handle_page
-from urllib.parse import parse_qsl
-
-qd = dict(parse_qsl(sys.argv[1]))
-print(handle_page(qd))
