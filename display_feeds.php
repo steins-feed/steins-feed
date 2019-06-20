7059e5c788824a8a780497b52d6eaae4af29ed03
Hans Yu <hans.yu@outlook.de>
2019-06-21 00:44:06 +0200

    Fix Display Feeds bug.

--- display_feeds.php
+++ display_feeds.php
@@ -12,14 +12,15 @@ $post_query = http_build_query($_POST);
 $python_cmd = <<<EOT
 import sys
 
-from steins_server import handle_display_feeds, handle_page
+from steins_feed import steins_generate_page
+from steins_server import handle_display_feeds
 from urllib.parse import parse_qsl
 
 qd = dict(parse_qsl(sys.argv[1], keep_blank_values=True))
 handle_display_feeds(qd)
-print(handle_page(qd))
+print(steins_generate_page(qd['user'], qd['lang'], int(qd['page'])))
 EOT;
 $bash_cmd = "env PYTHONIOENCODING=UTF-8 python3 -c \"$python_cmd\" \"$post_query\"";
-// system($bash_cmd . ' >> display_feeds.log 2>&1'); // DEBUG.
+// system($bash_cmd . ' > display_feeds.log 2>&1'); // DEBUG.
 system($bash_cmd);
 ?>
