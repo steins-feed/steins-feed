16273496446447bcbdd285e75099789d1d2fd4e0
Hans Yu <hans.yu@outlook.de>
2019-12-28 23:47:20 +0100

    Fix bug.

--- aux/update_db.py
+++ aux/update_db.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+
+dir_path = os.path.abspath(__file__)
+dir_path = os.path.dirname(dir_path)
+dir_path = os.path.join(dir_path, '..')
+dir_path = os.path.abspath(dir_path)
+
+sys.path.append(dir_path)
+
 from steins_feed import steins_update
 
 title_pattern = ""
