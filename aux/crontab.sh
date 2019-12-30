dfa73e72299ef9d3b6996d23f55366c4115b0dcf
Hans Yu <hans.yu@outlook.de>
2019-12-30 16:34:14 +0100

    Update crontab. Run update_db.py and do_magic.py on separate schedules.

--- aux/crontab.sh
+++
@@ -1,2 +0,0 @@
-python3 /var/www/html/steins-feed/aux/update_db.py >& /var/www/html/steins-feed/steins_feed.log
-python3 /var/www/html/steins-feed/aux/do_magic.py >& /var/www/html/steins-feed/steins_magic.log
