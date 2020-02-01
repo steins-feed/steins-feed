464fd59cda78f7159da4bd85380d1e04ccac4952
Hans Yu <hans.yu@outlook.de>
2020-02-01 12:38:30 +0100

    Parameterize connect_db.py to log into other database files.

--- aux/connect_db.py
+++ aux/connect_db.py
@@ -11,6 +11,10 @@ dir_path = os.path.abspath(dir_path)
 
 sys.path.append(dir_path)
 
-conn = sqlite3.connect("steins.db", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+file_name = "steins.db"
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+
+conn = sqlite3.connect(file_name, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 conn.row_factory = sqlite3.Row
 c = conn.cursor()
