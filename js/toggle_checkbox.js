c02d749fbe7994e4526d2232cb6e1b39493a56bd
Hans Yu <hans.yu@outlook.de>
2020-04-21 20:58:08 +0000

    Improve checkboxes in side navigation.

---
+++ js/toggle_checkbox.js
@@ -0,0 +1,25 @@
+function toggle_checkbox(fieldset_id) {
+    var stat = document.getElementById(fieldset_id);
+    var inputs = stat.getElementsByTagName("input");
+    if (inputs[0].checked) {
+        for (let input_it of inputs) {
+            input_it.checked = true;
+        }
+    } else {
+        for (let input_it of inputs) {
+            input_it.checked = false;
+        }
+    }
+}
+
+function prove_checkbox(fieldset_id) {
+    var stat = document.getElementById(fieldset_id);
+    var inputs = stat.getElementsByTagName("input");
+    for (let input_ct = 1; input_ct < inputs.length; input_ct++) {
+        if (!inputs[input_ct].checked) {
+            inputs[0].checked = false;
+            return;
+        }
+    }
+    inputs[0].checked = true;
+}
--- php_include/sidenav.php
+++ php_include/sidenav.php
@@ -23,41 +23,79 @@ close
 </i>
 </span>
 </nav>
+<script src="/steins-feed/js/toggle_checkbox.js" defer></script>
 <section>
 <form action="/steins-feed/index.php" method="get">
-<?php if (count($tags_disp)):?>
-<p>Tags:</p>
-<?php
-    foreach ($tags_disp as $tag_ct => $tag_it):
-        if (in_array($tag_it, $tags)):
-?>
-<input name="tag_<?php echo $tag_ct;?>" value="<?php echo $tag_it;?>" type="checkbox" checked><?php echo $tag_it;?><br>
+<?php if (!empty($tags_disp)):?>
+<p>
+<fieldset id="input_tag">
+<legend>Tags</legend>
+<label>
+<?php if (count($tags) >= count($tags_disp)):?>
+<input type="checkbox" onclick="toggle_checkbox('input_tag')" checked>
+<?php else:?>
+<input type="checkbox" onclick="toggle_checkbox('input_tag')">
+<?php endif;?>
+ALL TAGS
+<br>
+</label>
+<?php foreach ($tags_disp as $tag_ct => $tag_it):?>
+<label>
+<?php   if (in_array($tag_it, $tags)):?>
+<input name="tag_<?php echo $tag_ct;?>" onclick="prove_checkbox('input_tag')" value="<?php echo $tag_it;?>" type="checkbox" checked>
 <?php   else:?>
-<input name="tag_<?php echo $tag_ct;?>" value="<?php echo $tag_it;?>" type="checkbox"><?php echo $tag_it;?><br>
+<input name="tag_<?php echo $tag_ct;?>" onclick="prove_checkbox('input_tag')" value="<?php echo $tag_it;?>" type="checkbox">
 <?php
         endif;
-    endforeach;
-endif;
+    echo $tag_it . PHP_EOL;
 ?>
-<p>Languages:</p>
-<?php
-foreach ($langs_disp as $lang_ct => $lang_it):
-    if (in_array($lang_it, $langs)):
-?>
-<input name="lang_<?php echo $lang_ct;?>" value="<?php echo $lang_it;?>" type="checkbox" checked><?php echo $lang_it;?><br>
+</label>
+<br>
+<?php endforeach;?>
+</fieldset>
+</p>
+<?php endif;?>
+<?php if (!empty($langs_disp)):?>
+<p>
+<fieldset id="input_lang">
+<legend>Languages</legend>
+<label>
+<?php if (count($langs) >= count($langs_disp)):?>
+<input type="checkbox" onclick="toggle_checkbox('input_lang')" checked>
 <?php else:?>
-<input name="lang_<?php echo $lang_ct;?>" value="<?php echo $lang_it;?>" type="checkbox"><?php echo $lang_it;?><br>
+<input type="checkbox" onclick="toggle_checkbox('input_lang')">
+<?php endif?>
+ALL LANGUAGES
+<br>
+</label>
+<?php foreach ($langs_disp as $lang_ct => $lang_it):?>
+<label>
+<?php   if (in_array($lang_it, $langs)):?>
+<input name="lang_<?php echo $lang_ct;?>" onclick="prove_checkbox('input_lang')" value="<?php echo $lang_it;?>" type="checkbox" checked>
+<?php   else:?>
+<input name="lang_<?php echo $lang_ct;?>" onclick="prove_checkbox('input_lang')" value="<?php echo $lang_it;?>" type="checkbox">
 <?php
-    endif;
-endforeach;
+        endif;
+        echo $lang_it;
 ?>
-<p>Feed:</p>
+</label>
+<br>
+<?php endforeach;?>
+</fieldset>
+</p>
+<?php endif;?>
+<p>
+<fieldset>
+<legend>Feed</legend>
 <?php
 $feed_dict = array("Full", "Magic", "Surprise");
 $clf_dict = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/steins-feed/json/steins_magic.json"), true);
 $clf_key = key($clf_dict);
 $magic_exists = file_exists($_SERVER['DOCUMENT_ROOT'] . "/steins-feed/$user_id/$clf_key/clfs.pickle");
-foreach($feed_dict as $key) {
+foreach($feed_dict as $key):
+?>
+<label>
+<?php
     $feed_input = '<input name="feed" value="' . $key . '" type="radio"';
     if ($key == $feed) {
         $feed_input .= ' checked';
@@ -65,23 +103,32 @@ foreach($feed_dict as $key) {
     if (!$magic_exists) {
         $feed_input .= ' disabled';
     }
-    $feed_input .= '>' . $key . '<br>';
+    $feed_input .= '>';
     echo $feed_input . PHP_EOL;
-}
+    echo $key . PHP_EOL;
 ?>
+</label>
+<br>
+<?php endforeach;?>
+</fieldset>
+</p>
 <p>
+<fieldset>
 <input name="user" value="<?php echo $user;?>" type="hidden">
 <input name="page" value="<?php echo $page;?>" type="hidden">
 <input name="timeunit" value="<?php echo $timeunit;?>" type="hidden">
 <input name="new_timeunit" value="Day" type="submit">
 <input name="new_timeunit" value="Week" type="submit">
 <input name="new_timeunit" value="Month" type="submit">
+</fieldset>
 </p>
 </form>
 </section>
 <hr>
 <form action="/steins-feed/feed.php" method="get">
 <p>
+<fieldset>
+<legend>Feed</legend>
 <select name="feed">
 <?php
 $stmt = $db->prepare("SELECT * FROM Feeds Order BY Title COLLATE NOCASE");
@@ -98,10 +145,13 @@ foreach ($feeds_all as $feed_ct => $feed_it):
 </select>
 <input name="user" value="<?php echo $user;?>" type="hidden">
 <input type="submit" value="Edit">
+</fieldset>
 </p>
 </form>
 <form action="/steins-feed/tag.php" method="get">
 <p>
+<fieldset>
+<legend>Tag</legend>
 <select name="tag">
 <?php
 $stmt = $db->prepare("SELECT * FROM Tags WHERE UserID=:UserID ORDER BY Name COLLATE NOCASE");
@@ -118,6 +168,7 @@ foreach ($tags_all as $tag_ct => $tag_it):?>
 </select>
 <input name="user" value="<?php echo $user;?>" type="hidden">
 <input type="submit" value="Edit">
+</fieldset>
 </p>
 </form>
 </aside>
