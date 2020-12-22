36cc2d7147fa5fa1b5add9278ae72e2bae4fef95
Hans Yu <hans.yu@outlook.de>
2020-12-22 20:41:23 +0100

    First draft of read_feeds().

--- model/feeds.py
+++ model/feeds.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
+import feedparser
 from lxml import etree
 import sqlalchemy as sqla
 import sqlalchemy.sql as sql
@@ -25,11 +27,8 @@ def write_xml(f):
     conn = connect()
     feeds = get_table('Feeds')
 
-    sel = sql.select([feeds])
-    res = conn.execute(sel)
-
     root = etree.Element("root")
-    for row_it in res:
+    for row_it in conn.execute(sql.select([feeds])):
         title_it = etree.Element("title")
         title_it.text = row_it['Title']
         link_it = etree.Element("link")
@@ -45,3 +44,90 @@ def write_xml(f):
 
     s = etree.tostring(root, xml_declaration=True, pretty_print=True)
     f.write(s.decode())
+
+def read_feeds():
+    conn = connect()
+    feeds = get_table('Feeds')
+    items = get_table('Items')
+    handler = FeedHandler()
+
+    q = (sql
+        .select([feeds])
+        .order_by(sql.collate(feeds.c.Title, 'NOCASE'))
+    )
+    for feed_it in conn.execute(q):
+        d, status = handler.parse(feed_it['Link'])
+        for item_it in d['items']:
+            try:
+                item_title = handler.read_title(item_it)
+                item_link = handler.read_link(item_it)
+                item_time = handler.read_time(item_it)
+                item_summary = handler.read_summary(item_it)
+            except KeyError:
+                continue
+
+            ins = items.insert().values(
+                Title = item_title,
+                Link = item_link,
+                Published = item_time,
+                FeedID = feed_it['FeedID'],
+                Summary = item_summary
+            )
+            ins = ins.prefix_with("OR IGNORE", dialect='sqlite')
+            conn.execute(ins)
+
+class FeedHandler:
+    def read_title(self, item_it):
+        try:
+            item_title = item_it['title']
+            return item_title
+        except KeyError:
+            pass
+
+        #logger.error("No title.")
+        raise KeyError
+
+    def read_link(self, item_it):
+        try:
+            item_link = item_it['link']
+            return item_link
+        except KeyError:
+            pass
+
+        try:
+            item_link = item_it['links'][0]['href']
+            return item_link
+        except KeyError:
+            pass
+
+        #logger.error("No link for '{}'.".format(self.read_title(item_it)))
+        raise KeyError
+
+    def read_summary(self, item_it):
+        return item_it['summary']
+
+    def read_time(self, item_it):
+        try:
+            item_time = item_it['published_parsed']
+            item_time = datetime(*item_time[:6])
+            return item_time
+        except (KeyError, TypeError, ValueError):
+            pass
+
+        try:
+            item_time = item_it['updated_parsed']
+            item_time = datetime(*item_time[:6])
+            return item_time
+        except (KeyError, TypeError, ValueError):
+            pass
+
+        #logger.error("No time for '{}'.".format(self.read_title(item_it)))
+        raise KeyError
+
+    def parse(self, feed_link):
+        d = feedparser.parse(feed_link)
+        try:
+            status = d.status
+        except AttributeError:
+            status = -1
+        return d, status
--- tests/test_model_feeds.py
+++ tests/test_model_feeds.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from model.feeds import read_xml, write_xml
+from model.feeds import read_xml, write_xml, read_feeds
 
 with open("feeds.xml", 'r') as f:
     read_xml(f)
 with open("tests/feeds.xml", 'w') as f:
     write_xml(f)
+read_feeds()
