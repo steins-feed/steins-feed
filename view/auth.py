3f931177a1f595620e74f50efa149606609a7f60
Hans Yu <hans.yu@outlook.de>
2020-12-24 15:14:16 +0100

    User registration.

--- view/auth.py
+++ view/auth.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
-from flask_security import Security, SQLAlchemySessionUserDatastore, LoginForm
+from flask_security import Security, SQLAlchemySessionUserDatastore
+from flask_security import LoginForm, RegisterForm
 from flask_security import UserMixin, RoleMixin
+import json
+import os.path as os_path
 from sqlalchemy.orm import relationship, synonym
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 from model import get_model, get_session, get_table
+
+env_path = os_path.join(os_path.dirname(__file__), "env.json")
+with open(env_path, 'r') as f:
+    env_vars = json.load(f)
 
 user_datastore = None
 security = None
@@ -17,13 +24,15 @@ def get_user_datastore():
         User = get_model('Users', [UserMixin])
         Role = get_model('Roles', [RoleMixin])
 
+        User.id = synonym('UserID')
+        User.name = synonym('Name')
+        User.password = synonym('Password')
+        User.email = synonym('Email')
+        User.active = synonym('Active')
         User.roles = relationship(
                 'Roles',
                 secondary=get_table('Users2Roles'),
                 back_populates='users')
-        User.id = synonym('UserID')
-        User.password = synonym('Password')
-        User.active = synonym('Active')
 
         Role.users = relationship(
                 'Users',
@@ -36,13 +45,22 @@ def get_user_datastore():
 class ExtendedLoginForm(LoginForm):
     email = StringField('Username', [DataRequired()])
 
+class ExtendedRegisterForm(RegisterForm):
+    name = StringField('Username', [DataRequired()])
+
 def get_security(app):
     global security
     if not security:
-        app.config['SECRET_KEY'] = 'pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw'
-        app.config['SECURITY_PASSWORD_SALT'] = '146585145368132386173505678016728509634'
+        app.config['SECRET_KEY'] = env_vars['SECRET_KEY']
+        app.config['SECURITY_PASSWORD_SALT'] = env_vars['SECURITY_PASSWORD_SALT']
+
+        app.config['SECURITY_REGISTERABLE'] = True
+        app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
         app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = 'Name'
 
         user_datastore = get_user_datastore()
-        security = Security(app, user_datastore, login_form=ExtendedLoginForm)
+        security = Security(app, user_datastore,
+                login_form=ExtendedLoginForm,
+                register_form=ExtendedRegisterForm
+        )
     return security
