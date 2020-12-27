324b7572e2babbaa4e2b56a6674eeeb02bb6fc3c
Hans Yu <hans.yu@outlook.de>
2020-12-28 00:14:35 +0100

    Adjust scrolling.

--- static/js/next_item.js
+++ static/js/next_item.js
@@ -2,6 +2,9 @@ const main = document.getElementById('main');
 const articles = main.getElementsByTagName('article');
 const em = parseInt(getComputedStyle(main).getPropertyValue('font-size'));
 
+const topnavs = document.getElementsByClassName('topnav');
+const topnav_height = topnavs[0].offsetHeight;
+
 function next_item() {
     let lo = 0;
     let hi = articles.length - 1;
@@ -10,7 +13,7 @@ function next_item() {
         let mid = ((lo + hi) - (hi - lo) % 2) / 2;
         let article_it = articles[mid];
         let domRect = article_it.getBoundingClientRect();
-        if (domRect.y >= 2 * em + 1) {
+        if (domRect.y >= topnav_height + 1) {
             hi = mid;
         } else if (lo < mid) {
             lo = mid;
@@ -21,7 +24,7 @@ function next_item() {
 
     let article = articles[hi];
     article.scrollIntoView();
-    scrollBy(0, -2 * em);
+    scrollBy(0, -topnav_height);
 }
 
 function prev_item() {
@@ -32,7 +35,7 @@ function prev_item() {
         let mid = ((lo + hi) + (hi - lo) % 2) / 2;
         let article_it = articles[mid];
         let domRect = article_it.getBoundingClientRect();
-        if (domRect.y <= 2 * em - 1) {
+        if (domRect.y <= topnav_height - 1) {
             lo = mid;
         } else if (hi > mid) {
             hi = mid;
@@ -45,7 +48,7 @@ function prev_item() {
     let domRect = article.getBoundingClientRect();
     if (domRect.y <= 2 * em - 1) {
         article.scrollIntoView();
-        scrollBy(0, -2 * em);
+        scrollBy(0, -topnav_height);
     } else {
         scrollTo(0, 0);
     }
