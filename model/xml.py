33de44ffb3fdf0f31debae51932171e974ed0d26
Hans Yu <hans.yu@outlook.de>
2021-09-13 20:54:14 +0000

    Refactor read_xml().

--- model/xml.py
+++ model/xml.py
@@ -7,50 +7,51 @@ from . import engine, get_table
 from .schema import Language
 
 def read_xml(f, user_id=None, tag=None):
-    feeds = get_table('Feeds')
-    tags2feeds = get_table('Tags2Feeds')
-    tags = get_table('Tags')
+    t_feeds = get_table("Feeds")
+    t_tags2feeds = get_table("Tags2Feeds")
+    t_tags = get_table("Tags")
 
     tree = etree.parse(f)
     root = tree.getroot()
 
     rows = []
     for feed_it in root.xpath("feed"):
-        title = feed_it.xpath("title")[0].text
-        link = feed_it.xpath("link")[0].text
-        lang = Language(feed_it.xpath("lang")[0].text).name
+        rows.append({
+            "Title": feed_it.xpath("title")[0].text,
+            "Link": feed_it.xpath("link")[0].text,
+            "Language": Language(feed_it.xpath("lang")[0].text).name,
+        })
 
-        rows.append(dict(zip(
-                ['Title', 'Link', 'Language'],
-                [title, link, lang]
-        )))
+    q = t_feeds.insert()
+    q = q.prefix_with("OR IGNORE", dialect="sqlite")
 
-    q = feeds.insert()
-    q = q.prefix_with("OR IGNORE", dialect='sqlite')
     with engine.connect() as conn:
         conn.execute(q, rows)
 
     if user_id and tag:
+        q = t_tags.insert().values(
+            UserID=user_id,
+            Name=tag,
+        )
+        q = q.prefix_with("OR IGNORE", dialect="sqlite")
+
         with engine.connect() as conn:
-            q = tags.insert().values(
-                UserID=user_id,
-                Name=tag
-            )
-            q = q.prefix_with("OR IGNORE", dialect='sqlite')
             conn.execute(q)
 
-            q_select = sql.select([
-                feeds.c.FeedID,
-                tags.c.TagID
-            ]).where(sql.and_(
-                feeds.c.Title == sql.bindparam('Title'),
-                tags.c.UserID == user_id,
-                tags.c.Name == tag
-            ))
+        q_select = sql.select([
+            t_feeds.c.FeedID,
+            t_tags.c.TagID,
+        ]).where(sql.and_(
+            t_feeds.c.Title == sql.bindparam("Title"),
+            t_tags.c.UserID == user_id,
+            t_tags.c.Name == tag,
+        ))
 
-            q = tags2feeds.insert()
-            q = q.from_select([q_select.c.FeedID, q_select.c.TagID], q_select)
-            q = q.prefix_with("OR IGNORE", dialect='sqlite')
+        q = t_tags2feeds.insert()
+        q = q.from_select([q_select.c.FeedID, q_select.c.TagID], q_select)
+        q = q.prefix_with("OR IGNORE", dialect="sqlite")
+
+        with engine.connect() as conn:
             conn.execute(q, rows)
 
 def write_xml(f, user_id=None, tag=None):
