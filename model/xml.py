4ce5bc504a40829dfd5f4b0e766e329bf59badfd
Hans Yu <hans.yu@outlook.de>
2021-09-13 20:54:39 +0000

    Refactor write_xml().

--- model/xml.py
+++ model/xml.py
@@ -55,41 +55,46 @@ def read_xml(f, user_id=None, tag=None):
             conn.execute(q, rows)
 
 def write_xml(f, user_id=None, tag=None):
-    feeds = get_table('Feeds')
-    tags2feeds = get_table('Tags2Feeds')
-    tags = get_table('Tags')
+    t_feeds = get_table("Feeds")
+    t_tags2feeds = get_table("Tags2Feeds")
+    t_tags = get_table("Tags")
 
-    q = sql.select([feeds])
+    q = sql.select([t_feeds])
     if user_id and tag:
         q = q.select_from(
-            feeds.join(tags2feeds)
-                 .join(tags)
+            t_feeds.join(t_tags2feeds)
+                   .join(t_tags)
         )
         q = q.where(sql.and_(
-            tags.c.UserID == user_id,
-            tags.c.Name == tag
+            t_tags.c.UserID == user_id,
+            t_tags.c.Name == tag,
         ))
-    q = q.order_by(sql.collate(feeds.c.Title, 'NOCASE'))
+    q = q.order_by(sql.collate(t_feeds.c.Title, "NOCASE"))
+
     with engine.connect() as conn:
         rows = conn.execute(q).fetchall()
 
     root = etree.Element("root")
     for row_it in rows:
         title_it = etree.Element("title")
-        title_it.text = row_it['Title']
+        title_it.text = row_it["Title"]
+
         link_it = etree.Element("link")
-        link_it.text = row_it['Link']
+        link_it.text = row_it["Link"]
+
         lang_it = etree.Element("lang")
-        lang_it.text = Language[row_it['Language']].value
+        lang_it.text = Language[row_it["Language"]].value
 
         feed_it = etree.Element("feed")
         feed_it.append(title_it)
         feed_it.append(link_it)
         feed_it.append(lang_it)
+
         root.append(feed_it)
 
-    s = etree.tostring(root,
-            encoding='unicode',
-            pretty_print=True
+    s = etree.tostring(
+        root,
+        encoding="unicode",
+        pretty_print=True,
     )
     f.write(s)
