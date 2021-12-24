f15cd25449e518ab31817f0c60ca171732db02b8
Hans Yu <hans.yu@outlook.de>
2021-12-24 23:19:29 +0100

    First refactor of reset_magic().

--- aux/do_magic.py
+++ aux/do_magic.py
@@ -21,38 +21,53 @@ import magic
 import model
 from model import liked, recent
 from model.orm import users as orm_users
+from model.schema import feeds as schema_feeds
 
 logger = log.Logger(__name__, logging.INFO)
 
-def reset_magic(user_id=None, lang=None):
+def reset_magic(
+    user_id: int = None,
+    lang: schema_feeds.Language = None,
+):
+    """
+    Resets magic.
+
+    Args:
+      user_id: User ID.
+      lang: Language.
+    """
     feeds = model.get_table('Feeds')
     items = model.get_table('Items')
     magic = model.get_table('Magic')
 
     q = magic.delete()
     q_where = []
+
     if user_id:
         q_where.append(magic.c.UserID == user_id)
+
     if lang:
         if user_id:
-            q_cte = sqla.select([
-                items.c.ItemID
-            ]).select_from(
-                items.join(feeds)
-            ).where(
-                feeds.c.Language == lang.name
-            ).distinct()
-        else:
-            q_cte = sqla.select([
-                items.c.ItemID
-            ]).select_from(
+            q_cte = sqla.select(
+                items.c.ItemID,
+            ).select_from(
                 items.join(feeds)
                      .join(magic)
-            ).where(sqla.and_(
+            ).where(
                 feeds.c.Language == lang.name,
-                magic.c.UserID == user_id
-            )).distinct()
+                magic.c.UserID == user_id,
+            ).distinct()
+        else:
+            q_cte = sqla.select(
+                items.c.ItemID,
+            ).select_from(
+                items.join(feeds)
+            ).where(
+                feeds.c.Language == lang.name,
+            ).distinct()
+
         q_where.append(magic.c.ItemID.in_(q_cte))
+
     q = q.where(sqla.and_(*q_where))
     with model.get_connection() as conn:
         conn.execute(q)
