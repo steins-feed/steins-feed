4dfea8c7273c97bc681cf8b3f141fe7e63cabb0d
Hans Yu <hans.yu@outlook.de>
2021-12-24 23:47:13 +0100

    Prettify aux/.

--- aux/do_magic.py
+++ aux/do_magic.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""Retrains classifiers."""
+
 import collections
 import logging
 import os
--- aux/util.py
+++ aux/util.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
+"""Interaction with file system."""
+
+import datetime
 import os
 
-def is_up_to_date(file_path: str, datetime_ref: datetime) -> bool:
+def is_up_to_date(file_path: str, datetime_ref: datetime.datetime) -> bool:
+    """
+    Checks whether file is more up to date than a given datetime.
+
+    Args:
+      file_path: File path.
+      datetime_ref: Given datetime.
+    """
     if not os.path.exists(file_path):
         return False
 
     file_timestamp = os.stat(file_path).st_mtime
-    file_datetime = datetime.utcfromtimestamp(file_timestamp)
+    file_datetime = datetime.datetime.utcfromtimestamp(file_timestamp)
     return file_datetime > datetime_ref
 
 def mkdir_p(dir_path: str):
