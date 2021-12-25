4fc5d039d5413228cd430077df6de42db74fc675
Hans Yu <hans.yu@outlook.de>
2021-12-25 13:34:40 +0100

    Refactor upsert_like().

--- view/home/db.py
+++ view/home/db.py
@@ -5,32 +5,36 @@ import sqlalchemy as sqla
 import model
 from model.schema import items as schema_items
 
-def upsert_like(user_id, item_id, like_val):
+def upsert_like(
+    user_id: int,
+    item_id: int,
+    like_val: schema_items.Like,
+):
     like = model.get_table('Like')
 
-    q = sqla.select([
-            like
-    ]).where(sqla.and_(
-            like.c.UserID == user_id,
-            like.c.ItemID == item_id
-    ))
+    q = sqla.select(
+        like,
+    ).where(
+        like.c.UserID == user_id,
+        like.c.ItemID == item_id,
+    )
     with model.get_connection() as conn:
         res = conn.execute(q).fetchone()
 
     if res:
         score = schema_items.Like[res['Score']]
         q = like.update().values(
-               Score=sqla.bindparam("score")
-        ).where(sqla.and_(
-               like.c.UserID == user_id,
-               like.c.ItemID == item_id
-        ))
+            Score=sqla.bindparam("score")
+        ).where(
+            like.c.UserID == user_id,
+            like.c.ItemID == item_id,
+        )
     else:
         score = schema_items.Like.MEH
         q = like.insert().values(
-                UserID=user_id,
-                ItemID=item_id,
-                Score=sqla.bindparam("score")
+            UserID=user_id,
+            ItemID=item_id,
+            Score=sqla.bindparam("score"),
         )
 
     with model.get_connection() as conn:
