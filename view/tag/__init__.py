cd7cb01dfb1053cdd68baf72edd3bcb6fbc2141e
Hans Yu <hans.yu@outlook.de>
2021-12-26 18:48:28 +0100

    Type hints.

--- view/tag/__init__.py
+++ view/tag/__init__.py
@@ -14,22 +14,23 @@ bp = flask.Blueprint("tag", __name__, url_prefix="/tag")
 
 @bp.route("")
 @flask_security.auth_required()
-def tag():
+def tag() -> flask.Response:
     tag_id = flask.request.args.get('tag', type=int)
     with model.get_session() as session:
         tag = session.get(orm_feeds.Tag, tag_id)
 
-    return flask.render_template("tag.html",
-            **req.base_context(),
-            topnav_title=tag.Name,
-            tag_row=tag,
-            feeds_lang=db.feeds_lang(tag),
-            feeds_lang_not=db.feeds_lang(tag, False),
+    return flask.render_template(
+        "tag.html",
+        **req.base_context(),
+        topnav_title = tag.Name,
+        tag_row = tag,
+        feeds_lang = db.feeds_lang(tag),
+        feeds_lang_not = db.feeds_lang(tag, False),
     )
 
 @bp.route("/toggle_feeds", methods=['POST'])
 @flask_security.auth_required()
-def toggle_feeds():
+def toggle_feeds() -> flask.Response:
     tag_id = flask.request.form.get('tag_id', type=int)
     tagged = flask.request.form.getlist('tagged', type=int)
     untagged = flask.request.form.getlist('untagged', type=int)
