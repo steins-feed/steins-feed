795f7440e475e08691f077ba5e7d9d869137fca7
Hans Yu <hans.yu@outlook.de>
2021-12-26 19:26:27 +0100

    Order by date.

--- view/overview/db.py
+++ view/overview/db.py
@@ -8,6 +8,7 @@ from model.orm import items as orm_items
 from model.orm import feeds as orm_feeds
 from model.orm import users as orm_users
 from model.orm.items import filter as items_filter
+from model.orm.items import order as items_order
 from model.schema import feeds as schema_feeds
 from model.schema import items as schema_items
 from model.utils import all_langs_feeds
@@ -18,13 +19,12 @@ def likes_lang(
 ) -> typing.Dict[schema_feeds.Language, typing.List[orm_items.Item]]:
     q = sqla.select(
         orm_items.Item
-    ).order_by(
-        sqla.desc(orm_items.Item.Published),
     ).options(
         sqla.orm.contains_eager(orm_items.Item.feed),
     )
     q = items_filter.filter_lang(q, sqla.bindparam("lang"))
     q = items_filter.filter_like(q, score, user)
+    q = items_order.order_date(q)
 
     res = dict()
     with model.get_session() as session:
