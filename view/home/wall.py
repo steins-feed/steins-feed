c22304080a3a5c4103e71319f21d5dc057083e9f
Hans Yu <hans.yu@outlook.de>
2021-12-26 14:31:06 +0100

    Refactor order_wall().

--- view/home/wall.py
+++ view/home/wall.py
@@ -3,6 +3,7 @@
 import enum
 import sqlalchemy as sqla
 
+from model.orm import users as orm_users
 from model.orm.items import load as items_load
 from model.orm.items import order as items_order
 
@@ -14,11 +15,11 @@ class WallMode(enum.Enum):
 def order_wall(
     q: sqla.sql.Select,
     wall_mode: WallMode,
-    user_id: int,
+    user: orm_users.User,
 ) -> sqla.sql.Select:
     if wall_mode == WallMode.MAGIC:
-        q = items_order.order_magic(q, user_id)
-        q = items_load.load_magic(q, user_id, magic_joined=True)
+        q = items_order.order_magic(q, user)
+        q = items_load.load_magic(q, user, magic_joined=True)
     else:
         q = items_order.order_date(q)
 
