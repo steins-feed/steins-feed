763ee95426d865895581a7fea711cc58da24ad31
Hans Yu <hans.yu@outlook.de>
2021-12-26 14:55:25 +0100

    Refactor import.

--- view/feed/__init__.py
+++ view/feed/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Blueprint, request, render_template, redirect, url_for
-from flask_security import auth_required, current_user
+import flask
+import flask_security
 import sqlalchemy as sqla
 
-from .req import base_context
 from model import get_session
 from model.orm.feeds import Feed, Tag
 from model.orm.users import User
@@ -12,13 +11,17 @@ from model.schema.feeds import Language
 from model.utils.custom import upsert_feed, upsert_display
 from model.utils.custom import delete_tags_tagged, insert_tags_untagged
 
-bp = Blueprint("feed", __name__, url_prefix="/feed")
+from ..req import base_context
+
+bp = flask.Blueprint("feed", __name__, url_prefix="/feed")
 
 @bp.route("")
-@auth_required()
+@flask_security.auth_required()
 def feed(feed_id=None):
     if not feed_id:
-        feed_id = request.args.get("feed_id", type=int)
+        feed_id = flask.request.args.get("feed_id", type=int)
+
+    user = flask_security.current_user
 
     with get_session() as session:
         feed_row = session.get(
@@ -26,41 +29,43 @@ def feed(feed_id=None):
             feed_id,
             options=[
                 sqla.orm.selectinload(Feed.users.and_(
-                    User.UserID == current_user.UserID,
+                    User.UserID == user.UserID,
                 )),
             ],
         )
 
-    return render_template("feed.html",
+    return flask.render_template("feed.html",
         **base_context(),
         topnav_title=feed_row.Title,
         feed_row=feed_row,
         langs_all=[e for e in Language],
         lang_default=Language.ENGLISH,
-        feed_tags=feed_tags(current_user.UserID, feed_id),
-        feed_tags_not=feed_tags(current_user.UserID, feed_id, False),
+        feed_tags=feed_tags(user.UserID, feed_id),
+        feed_tags_not=feed_tags(user.UserID, feed_id, False),
     )
 
 @bp.route("/update_feed", methods=["POST"])
-@auth_required()
+@flask_security.auth_required()
 def update_feed():
-    feed_id = request.form.get("feed", type=int)
-    title = request.form.get("title")
-    link = request.form.get("link")
-    lang = Language[request.form.get("lang")]
-    display = request.form.get("display", type=int)
+    feed_id = flask.request.form.get("feed", type=int)
+    title = flask.request.form.get("title")
+    link = flask.request.form.get("link")
+    lang = Language[flask.request.form.get("lang")]
+    display = flask.request.form.get("display", type=int)
+
+    user = flask_security.current_user
 
     upsert_feed(feed_id, title, link, lang)
-    upsert_display(current_user.UserID, [feed_id], display)
+    upsert_display(user.UserID, [feed_id], display)
 
-    return redirect(url_for("feed.feed", feed_id=feed_id))
+    return flask.redirect(flask.url_for("feed.feed", feed_id=feed_id))
 
 @bp.route("/toggle_tags", methods=["POST"])
-@auth_required()
+@flask_security.auth_required()
 def toggle_tags():
-    feed_id = request.form.get("feed_id", type=int)
-    tagged = request.form.getlist("tagged", type=int)
-    untagged = request.form.getlist("untagged", type=int)
+    feed_id = flask.request.form.get("feed_id", type=int)
+    tagged = flask.request.form.getlist("tagged", type=int)
+    untagged = flask.request.form.getlist("untagged", type=int)
 
     delete_tags_tagged(feed_id, tagged)
     insert_tags_untagged(feed_id, untagged)
