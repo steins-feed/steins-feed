4d5bf19b1365d442fa0f7f003f3a69dcdc431e70
Hans Yu <hans.yu@outlook.de>
2021-12-26 21:33:11 +0100

    Refactor auth.py.

--- view/auth.py
+++ view/auth.py
@@ -1,62 +1,73 @@
 #!/usr/bin/env python3
 
-import os
-
 import bleach
-from dotenv import load_dotenv
-from flask_security import Security, SQLAlchemySessionUserDatastore
-from flask_security import LoginForm, RegisterForm
-from flask_security import UserMixin, RoleMixin
-from sqlalchemy.orm import relationship, synonym
-from wtforms import StringField
-from wtforms.validators import DataRequired
+import dotenv
+import flask
+import flask_security
+import os
+import sqlalchemy as sqla
+import wtforms
 
-from model import Session
-from model.orm import users
+import model
+from model.orm import users as orm_users
 
-load_dotenv(os.path.join(
+env_path = os.path.join(
     os.path.dirname(__file__),
     os.pardir,
     ".env",
-))
+)
+dotenv.load_dotenv(env_path)
 
-class FSUser(users.User, UserMixin):
-    id = synonym('UserID')
-    name = synonym('Name')
-    password = synonym('Password')
-    email = synonym('Email')
-    active = synonym('Active')
+class FSUser(orm_users.User, flask_security.UserMixin):
+    id = sqla.orm.synonym("UserID")
+    name = sqla.orm.synonym("Name")
+    password = sqla.orm.synonym("Password")
+    email = sqla.orm.synonym("Email")
+    active = sqla.orm.synonym("Active")
 
-class FSRole(users.Role, RoleMixin):
+class FSRole(orm_users.Role, flask_security.RoleMixin):
     ...
 
-def get_user_datastore():
+def get_user_datastore() -> flask_security.SQLAlchemySessionUserDatastore:
     global user_datastore
-    if 'user_datastore' not in globals():
-        user_datastore = SQLAlchemySessionUserDatastore(Session, FSUser, FSRole)
+    if "user_datastore" not in globals():
+        user_datastore = flask_security.SQLAlchemySessionUserDatastore(
+            model.Session,
+            FSUser,
+            FSRole,
+        )
     return user_datastore
 
-class ExtendedLoginForm(LoginForm):
-    email = StringField('Username', [DataRequired()])
+class ExtendedLoginForm(flask_security.LoginForm):
+    email = wtforms.StringField(
+        "Username",
+        [wtforms.validators.DataRequired()],
+    )
 
-class ExtendedRegisterForm(RegisterForm):
-    name = StringField('Username', [DataRequired()])
+class ExtendedRegisterForm(flask_security.RegisterForm):
+    name = wtforms.StringField(
+        "Username",
+        [wtforms.validators.DataRequired()],
+    )
 
-def get_security(app):
+def get_security(app: flask.Flask) -> flask_security.Security:
     global security
-    if 'security' not in globals():
-        app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-        app.config['SECURITY_PASSWORD_SALT'] = os.getenv('SECURITY_PASSWORD_SALT')
+    if "security" not in globals():
+        app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+        app.config["SECURITY_PASSWORD_SALT"] = os.getenv("SECURITY_PASSWORD_SALT")
 
-        app.config['SECURITY_REGISTERABLE'] = True
-        app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
-        app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = [{
-            "Name": {"mapper": lambda x: bleach.clean(x, strip=True)}
-        }]
+        app.config["SECURITY_REGISTERABLE"] = True
+        app.config["SECURITY_SEND_REGISTER_EMAIL"] = False
+        app.config["SECURITY_USER_IDENTITY_ATTRIBUTES"] = [
+            {"Name": {"mapper": lambda x: bleach.clean(x, strip=True)}}
+        ]
 
         user_datastore = get_user_datastore()
-        security = Security(app, user_datastore,
-                login_form=ExtendedLoginForm,
-                register_form=ExtendedRegisterForm
+        security = flask_security.Security(
+            app,
+            user_datastore,
+            login_form=ExtendedLoginForm,
+            register_form=ExtendedRegisterForm,
         )
     return security
+
