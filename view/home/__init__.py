095b6bdf0bb293c3f82b962791a68aa9965181c7
Hans Yu <hans.yu@outlook.de>
2021-12-26 12:50:24 +0100

    Refactor home view.

--- view/home/__init__.py
+++ view/home/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Blueprint, request, render_template
-from flask_security import auth_required, current_user
-import sqlalchemy as sqla
+import flask
+import flask_security
 
 import magic
 from magic import io as magic_io
@@ -15,11 +14,11 @@ from . import unit
 from . import wall
 from .. import req
 
-bp = Blueprint("home", __name__, url_prefix="/home")
+bp = flask.Blueprint("home", __name__, url_prefix="/home")
 bp.add_app_template_filter(util.clean_summary, "clean")
 
 @bp.route("")
-@auth_required()
+@flask_security.auth_required()
 def home():
     r_wall = req.get_wall()
     r_langs = req.get_langs()
@@ -27,6 +26,7 @@ def home():
     r_tags = req.get_tags()
     r_timeunit = req.get_timeunit()
 
+    user = flask_security.current_user
     last_hour = recent.last_updated()
     last_hour = last_hour.replace(
         minute=0,
@@ -38,9 +38,9 @@ def home():
     finish_time = unit.increment_to(start_time, r_timeunit)
 
     if r_wall == wall.WallMode.MAGIC:
-        for lang_it in magic_io.trained_languages(current_user):
+        for lang_it in magic_io.trained_languages(user):
             new_items = db.unscored_items(
-                current_user.UserID,
+                user.UserID,
                 lang_it,
                 r_tags,
                 start_time,
@@ -50,11 +50,11 @@ def home():
             if len(new_items) == 0:
                 continue
 
-            new_scores = magic.compute_score(current_user.UserID, lang_it, new_items)
-            db.upsert_magic(current_user.UserID, new_items, new_scores)
+            new_scores = magic.compute_score(user.UserID, lang_it, new_items)
+            db.upsert_magic(user.UserID, new_items, new_scores)
 
     page_items = db.updated_items(
-        current_user.UserID,
+        user.UserID,
         r_langs,
         r_tags,
         start_time,
@@ -63,7 +63,7 @@ def home():
         r_wall,
     )
 
-    return render_template("index.html",
+    return flask.render_template("index.html",
         **req.base_context(),
         topnav_title=unit.format_to(r_page, r_timeunit),
         last_updated=last_hour,
@@ -71,14 +71,20 @@ def home():
         enum_like=schema_items.Like,
     )
 
-@bp.route("/like", methods=['POST'])
-@auth_required()
-def like(like_val = schema_items.Like.UP):
-    db.upsert_like(current_user.UserID, int(request.form.get('id')), like_val)
-    return ("", 200)
+@bp.route("/like", methods=["POST"])
+@flask_security.auth_required()
+def like(
+    like_val: schema_items.Like = schema_items.Like.UP,
+) -> flask.Response:
+    user = flask_security.current_user
+    item_id = flask.request.form.get("id", type=int)
 
-@bp.route("/dislike", methods=['POST'])
-@auth_required()
-def dislike():
+    db.upsert_like(user.UserID, item_id, like_val)
+
+    return "", 200
+
+@bp.route("/dislike", methods=["POST"])
+@flask_security.auth_required()
+def dislike() -> flask.Response:
     return like(schema_items.Like.DOWN)
 
