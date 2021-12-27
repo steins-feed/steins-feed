533d16a0a59a7f45b14045f221ef735e6fed2e97
Hans Yu <hans.yu@outlook.de>
2021-12-27 11:22:25 +0100

    First draft of sample_wall().

--- view/home/wall.py
+++ view/home/wall.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
 import enum
+import random
 import sqlalchemy as sqla
+import typing
 
+from model.orm import items as orm_items
 from model.orm import users as orm_users
 from model.orm.items import load as items_load
 from model.orm.items import order as items_order
 
 class WallMode(enum.Enum):
     CLASSIC = "Classic"
+    RANDOM = "Random"
     MAGIC = "Magic"
     SURPRISE = "Surprise"
 
@@ -24,4 +28,17 @@ def order_wall(
         q = items_order.order_date(q)
 
     return q
+
+def sample_wall(
+    items: typing.List[orm_items.Item],
+    wall_mode: WallMode,
+) -> typing.List[orm_items.Item]:
+    new_items = items
+
+    if wall_mode == WallMode.RANDOM:
+        k = random.choices(range(len(items)), k=10)
+        k = sorted(k)
+        new_items = [items[i] for i in k]
+
+    return new_items
 
