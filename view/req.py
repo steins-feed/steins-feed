04b19ab7a3131478466eac1126afe9ea8808c004
Hans Yu <hans.yu@outlook.de>
2021-12-27 01:10:01 +0100

    Refactor import.

--- view/req.py
+++ view/req.py
@@ -3,14 +3,14 @@
 import datetime
 import os
 
-from flask import request
-from flask_security import current_user
+import flask
+import flask_security
 import sqlalchemy as sqla
 
-from model import get_session
+import model
 from model.orm import feeds as orm_feeds
 from model.orm import users as orm_users
-from model.schema.feeds import Language
+from model.schema import feeds as schema_feeds
 from view.feed import db as feed_db
 from view.home import unit as home_unit
 from view.home import wall as home_wall
@@ -18,7 +18,7 @@ from view.overview import db as overview_db
 from view.tag import db as tag_db
 
 def get_wall():
-    wall_name = request.args.get(
+    wall_name = flask.request.args.get(
         "feed",
         default = home_wall.WallMode.CLASSIC.name,
         type=str,
@@ -26,9 +26,12 @@ def get_wall():
     return home_wall.WallMode[wall_name]
 
 def get_langs():
-    res = [Language[e] for e in request.args.getlist('lang')]
-    if set(overview_db.all_langs(current_user)) <= set(res):
+    user = flask_security.current_user
+
+    res = [schema_feeds.Language[e] for e in flask.request.args.getlist('lang')]
+    if set(overview_db.all_langs(user)) <= set(res):
         res = []
+
     return res
 
 def get_page():
@@ -36,7 +39,7 @@ def get_page():
     current_time = home_unit.round_to(current_time, get_timeunit())
 
     if get_timeunit() == get_timeunit(old=True):
-        s = request.args.get('page', default=None, type=str)
+        s = flask.request.args.get('page', default=None, type=str)
         if s:
             return datetime.datetime.fromisoformat(s)
         else:
@@ -45,16 +48,18 @@ def get_page():
         return current_time
 
 def get_tags():
-    tags_name = request.args.getlist("tag")
+    user = flask_security.current_user
+
+    tags_name = flask.request.args.getlist("tag")
 
     q = sqla.select(
         orm_feeds.Tag,
     ).where(
-        orm_feeds.Tag.UserID == current_user.UserID,
+        orm_feeds.Tag.UserID == user.UserID,
         orm_feeds.Tag.Name.in_(tags_name),
     )
 
-    with get_session() as session:
+    with model.get_session() as session:
         return [e[0] for e in session.execute(q)]
 
 def get_timeunit(old: bool=False):
@@ -65,7 +70,7 @@ def get_timeunit(old: bool=False):
         k = "timeunit_new"
         v = get_timeunit(old=True).name
 
-    unit_name = request.args.get(
+    unit_name = flask.request.args.get(
         k,
         default = v,
         type=str,
@@ -74,6 +79,7 @@ def get_timeunit(old: bool=False):
     return home_unit.UnitMode[unit_name]
 
 def base_context():
+    user = flask_security.current_user
     context = dict()
 
     # topnav.html.
@@ -86,19 +92,19 @@ def base_context():
     context['tags'] = get_tags()
 
     # sidenav.html.
-    context['langs_disp'] = overview_db.all_langs(current_user)
-    context['tags_disp'] = feed_db.all_tags(current_user, display=True)
+    context['langs_disp'] = overview_db.all_langs(user)
+    context['tags_disp'] = feed_db.all_tags(user, display=True)
 
     # sidenav.html.
     context['feeds_all']=tag_db.all_feeds()
-    context['tags_all']=feed_db.all_tags(current_user)
+    context['tags_all']=feed_db.all_tags(user)
 
     # sidenav.html.
     dir_path = os.path.normpath(os.path.join(
         os.path.dirname(__file__),
         os.pardir,
         "clf.d",
-        str(current_user.UserID)
+        str(user.UserID)
     ))
     context['magic_exists'] = os.path.isdir(dir_path)
 
