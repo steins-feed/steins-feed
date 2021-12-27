c7897339eea955681590d8fb3dafb3b314b22ed0
Hans Yu <hans.yu@outlook.de>
2021-12-27 11:50:59 +0100

    Custom choices().

--- view/home/wall.py
+++ view/home/wall.py
@@ -41,14 +41,30 @@ def sample_wall(
     new_items = items
 
     if wall_mode == WallMode.RANDOM:
-        k = random.choices(range(len(items)), k=10)
-        k = sorted(k)
-        new_items = [items[i] for i in k]
+        new_items = choices(items, 10)
     elif wall_mode == WallMode.SURPRISE:
         w = [item_it.magic[0].Score for item_it in items]
-        k = random.choices(range(len(items)), weights=w, k=10)
-        k = sorted(k)
-        new_items = [items[i] for i in k]
+        new_items = choices(items, 10, w)
 
     return new_items
+
+def choices(
+    items: typing.List[orm_items.Item],
+    k: int,
+    weights: typing.List[float] = None,
+) -> typing.List[orm_items.Item]:
+    n = len(items)
+    if n <= k:
+        return items
+
+    keys = set()
+    while len(keys) < k:
+        new_keys = random.choices(
+            range(n),
+            weights = weights,
+            k = k - len(keys),
+        )
+        keys |= set(new_keys)
+
+    return [items[key_it] for key_it in sorted(keys)]
 
