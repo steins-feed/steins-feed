f2ea9069234c6ad71a53e028020c33f4bcf8ab4e
Hans Yu <hans.yu@outlook.de>
2022-01-02 18:01:18 +0100

    Refactor like.js.

--- static/js/like.js
+++ static/js/like.js
@@ -1,37 +1,49 @@
 function like(button_id) {
-    var xmlhttp = new XMLHttpRequest();
+    const xmlhttp = new XMLHttpRequest();
+
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var stat_like = document.getElementById('like_' + button_id);
-            var stat_dislike = document.getElementById('dislike_' + button_id);
+            const stat_like = document.getElementById('like_' + button_id);
+            const stat_dislike = document.getElementById('dislike_' + button_id);
+
             if (stat_like.className == 'liked') {
                 stat_like.className = 'like';
             } else {
                 stat_like.className = 'liked';
             }
+
             stat_dislike.className = 'dislike';
         }
     };
-    xmlhttp.open("POST", home_ep + "/like", true);
-    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlhttp.send("id=" + button_id);
+
+    const fd = new FormData();
+    fd.append("id", button_id);
+
+    xmlhttp.open("POST", "/home/like");
+    xmlhttp.send(fd);
 }
 
 function dislike(button_id) {
-    var xmlhttp = new XMLHttpRequest();
+    const xmlhttp = new XMLHttpRequest();
+
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var stat_like = document.getElementById('like_' + button_id);
-            var stat_dislike = document.getElementById('dislike_' + button_id);
+            const stat_like = document.getElementById('like_' + button_id);
+            const stat_dislike = document.getElementById('dislike_' + button_id);
+
+            stat_like.className = 'like';
+
             if (stat_dislike.className == 'disliked') {
                 stat_dislike.className = 'dislike';
             } else {
                 stat_dislike.className = 'disliked';
             }
-            stat_like.className = 'like';
         }
     };
-    xmlhttp.open("POST", home_ep + "/dislike", true);
-    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlhttp.send("id=" + button_id);
+
+    const fd = new FormData();
+    fd.append("id", button_id);
+
+    xmlhttp.open("POST", "/home/dislike");
+    xmlhttp.send(fd);
 }
